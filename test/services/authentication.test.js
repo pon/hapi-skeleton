94c0a9474d0cd063b5bca16f94fbd5791809e793
Peter Nagel <pnagel5@gmail.com>
2015-05-25 20:06:24 -0700

    moved validate to server method

--- test/services/authentication.test.js
+++ test/services/authentication.test.js
@@ -26,7 +26,7 @@ lab.experiment('authentication', function () {
 
     lab.test('should return valid credentials for good api key', function (done) {
 
-      server.plugins.authentication.validate('api_key', null, function (err, valid, credentials) {
+      server.methods.authentication.validate('api_key', null, function (err, valid, credentials) {
         expect(err).to.be.null();
         expect(valid).to.be.true();
         expect(credentials.api_key).to.equal('api_key');
@@ -36,7 +36,7 @@ lab.experiment('authentication', function () {
 
     lab.test('should reject for a bad api key', function (done) {
 
-      server.plugins.authentication.validate('asdf', null, function (err, valid) {
+      server.methods.authentication.validate('asdf', null, function (err, valid) {
         expect(err).to.be.an.instanceof(server.plugins.errors.api.UnauthorizedUser);
         done();
       });
